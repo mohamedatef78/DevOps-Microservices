77a41276bc033665af1bed2c2ce5698ebbe431be
mohamedatef78 <76219441+mohamedatef78@users.noreply.github.com>
2021-02-11 11:02:00 +0200

    push

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,17 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=mohamedatef96/api-prediction
+TAG=api-prediction
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run $TAG --image=$dockerpath --generator=run-pod/v1
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods --all-namespaces
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward pod/$TAG 8000:80
